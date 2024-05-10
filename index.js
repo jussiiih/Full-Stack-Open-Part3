aca1d08abfc9609c60bc2a0ef3464e6eee956aa7
Juha-Pekka Heino <juhapekka.heino@brightstraining.com>
2024-05-10 11:28:21 +0300

    tehtävä 3.10

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :r
 const cors = require('cors')
 app.use(cors())
 
+app.use(express.static('dist'))
+
 let phonebook = [
     {
         id: 1,
